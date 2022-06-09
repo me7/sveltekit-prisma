654e97322b92bd164b11a14c47e1155b855fa973
me7/ <gampolt@gmail.com>
2022-06-09 11:06:38 +0700

    type cast FormDataValue to String

--- src/routes/todos/index.ts
+++ src/routes/todos/index.ts
@@ -14,13 +14,12 @@ export async function post(event: RequestEvent) {
 	const form = await event.request.formData();
 	let todo = await db.todo.create({
 		data: {
-			text: form.get('text'),
+			text: String(form.get('text')),
 			created_at: new Date(),
 			done: false,
 			pending_delete: false
 		}
 	})
-	console.log(todo)
 	return {
 		body: {
 			todo
